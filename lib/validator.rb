924b5e688ba3bbb8adb1bde9bb015f36b87bd31d
21DP3EFrei <103174371+21DP3EFrei@users.noreply.github.com>
2023-12-22 13:23:24 +0000

    ver 3 (fix valid_row)

--- lib/validator.rb
+++ lib/validator.rb
@@ -9,10 +9,10 @@ class Validator
 
   def validate
     result =
-      if valid?
-        'Sudoku is valid.'
-      elsif incomplete?
+      if valid? && incomplete?
         'Sudoku is valid but incomplete.'
+      elsif valid?
+        'Sudoku is valid.'
       else
         'Sudoku is invalid.'
       end
@@ -26,37 +26,56 @@ class Validator
   def valid?
     rows = valid_rows?
     columns = valid_columns?
-  
+
     puts "Rows: #{rows}"
     puts "Columns: #{columns}"
-  
     rows && columns
   end
 
   # Check if the puzzle is incomplete
   def incomplete?
-    @puzzle_string.include?('0') && valid_rows? && valid_columns?
+    @puzzle_string.include?('0')
   end
 
   # Check if all rows are valid
   def valid_rows?
-    result = @puzzle_string.each_line.all? { |row| !contains_duplicates?(row.scan(/\d+/).map(&:to_i)) }
+    result = @puzzle_string.each_line.with_index.all? do |row, index|
+      row_values = row.scan(/\d+/).map(&:to_i)
+
+      # Exclude 0 when checking for duplicates
+      non_zero_values = row_values.reject { |value| value.zero? }
+      duplicates = non_zero_values.size > 1 && non_zero_values.size != non_zero_values.uniq.size
+
+      # Check if each number from 1 to 9 is present at most once in the row
+      contains_all_numbers = (1..9).all? { |num| row_values.count(num) <= 1 }
+
+      puts "Row #{index}: Non-zero values: #{non_zero_values}, Duplicates: #{duplicates ? 'Invalid' : 'Valid'}, Contains all numbers: #{contains_all_numbers} #{row_values}"
+
+      (!duplicates || non_zero_values.empty?) && contains_all_numbers && !contains_invalid_numbers?(row_values)
+    end
     result
-  end  
+  end
 
   # Check if all columns are valid
   def valid_columns?
     result = (0...9).all? do |col|
       column = @puzzle_string.each_line.map { |row| row.scan(/\d+/)[col].to_i }
-      duplicates = contains_duplicates?(column)
-      puts "Column #{col}: #{duplicates ? 'Invalid' : 'Valid'} #{column}"
-      !duplicates
+      # Exclude 0 when checking for duplicates
+      non_zero_values = column.reject { |value| value.zero? }
+      duplicates = non_zero_values.size > 1 && non_zero_values.size != non_zero_values.uniq.size
+
+      # Check if each number from 1 to 9 is present at most once in the column
+      contains_all_numbers = (1..9).all? { |num| column.count(num) <= 1 }
+
+      puts "Column #{col}: Non-zero values: #{non_zero_values}, Duplicates: #{duplicates ? 'Invalid' : 'Valid'}, Contains all numbers: #{contains_all_numbers} #{column}"
+
+      (!duplicates || non_zero_values.empty?) && contains_all_numbers
     end
     result
-  end  
+  end
 
-  # Check if an array contains duplicates
-  def contains_duplicates?(array)
-    array.compact.uniq.size != array.compact.size
+  # Check if an array contains invalid numbers
+  def contains_invalid_numbers?(array)
+    array.any? { |value| value < 1 || value > 9 }
   end
 end
