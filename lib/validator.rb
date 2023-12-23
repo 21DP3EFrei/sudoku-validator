dca6589f015fafae900e2509a96c745519c0ae4b
21DP3EFrei <103174371+21DP3EFrei@users.noreply.github.com>
2023-12-23 13:11:49 +0000

    ver 4 (need to fix subgroup to clear all tests)

--- lib/validator.rb
+++ lib/validator.rb
@@ -26,11 +26,15 @@ class Validator
   def valid?
     rows = valid_rows?
     columns = valid_columns?
-
+    subgroups = valid_subgroups?
+  
     puts "Rows: #{rows}"
     puts "Columns: #{columns}"
-    rows && columns
+    puts "Subgroups: #{subgroups}"
+  
+    rows && columns && subgroups
   end
+  
 
   # Check if the puzzle is incomplete
   def incomplete?
@@ -38,44 +42,61 @@ class Validator
   end
 
   # Check if all rows are valid
-  def valid_rows?
-    result = @puzzle_string.each_line.with_index.all? do |row, index|
-      row_values = row.scan(/\d+/).map(&:to_i)
+ # Check if all rows are valid
+def valid_rows?
+  result = @puzzle_string.each_line.with_index.all? do |row, index|
+    row_values = row.scan(/\d+/).map(&:to_i)
+    
+    # Exclude zeros when checking for duplicates
+    non_zero_values = row_values.reject { |value| value.zero? }
+    
+    invalid_numbers = non_zero_values.select { |value| value < 1 || value > 9 }
 
-      # Exclude 0 when checking for duplicates
-      non_zero_values = row_values.reject { |value| value.zero? }
-      duplicates = non_zero_values.size > 1 && non_zero_values.size != non_zero_values.uniq.size
+    duplicates = non_zero_values.size > 1 && non_zero_values.size != non_zero_values.uniq.size
+    contains_all_numbers = (1..9).all? { |num| row_values.count(num) <= 1 }
 
-      # Check if each number from 1 to 9 is present at most once in the row
-      contains_all_numbers = (1..9).all? { |num| row_values.count(num) <= 1 }
+    puts "Row #{index}: Non-zero values: #{non_zero_values}, Invalid Numbers: #{invalid_numbers}, Duplicates: #{duplicates ? 'Invalid' : 'Valid'}, Contains all numbers: #{contains_all_numbers} #{row_values}"
 
-      puts "Row #{index}: Non-zero values: #{non_zero_values}, Duplicates: #{duplicates ? 'Invalid' : 'Valid'}, Contains all numbers: #{contains_all_numbers} #{row_values}"
+    (!duplicates || non_zero_values.empty?) && invalid_numbers.empty? && contains_all_numbers
+  end
+  result
+end
 
-      (!duplicates || non_zero_values.empty?) && contains_all_numbers && !contains_invalid_numbers?(row_values)
+# Check if all columns are valid
+def valid_columns?
+  result = (0...9).all? do |col|
+    column = @puzzle_string.each_line.map { |row| row.scan(/\d+/)[col].to_i }
+    non_zero_values = column.reject { |value| value.zero? }
+
+    invalid_numbers = non_zero_values.select { |value| value < 1 || value > 9 }
+
+    duplicates = non_zero_values.size != non_zero_values.uniq.size
+    contains_all_numbers = (1..9).all? { |num| column.count(num) <= 1 }
+
+    puts "Column #{col}: Non-zero values: #{non_zero_values}, Invalid Numbers: #{invalid_numbers}, Duplicates: #{duplicates ? 'Invalid' : 'Valid'}, Contains all numbers: #{contains_all_numbers} #{column}"
+
+    (!duplicates || non_zero_values.empty?) && invalid_numbers.empty? && contains_all_numbers
+  end
+  result
+end
+
+  # Check if all subgroups are valid
+  def valid_subgroups?
+    result = (0..6).step(3).all? do |row|
+      (0..6).step(3).all? do |col|
+        subgroup = (0..2).flat_map do |i|
+          (0..2).map { |j| @puzzle_string[row + i][col + j].to_i }
+        end
+  
+        non_zero_values = subgroup.reject { |value| value.zero? }
+        duplicates = non_zero_values.size != non_zero_values.uniq.size
+  
+        puts "Subgroup: Non-zero values: #{non_zero_values}, Duplicates: #{duplicates ? 'Invalid' : 'Valid'}, Subgroup: #{subgroup}"
+  
+        !duplicates
+      end
     end
     result
   end
-
-  # Check if all columns are valid
-  def valid_columns?
-    result = (0...9).all? do |col|
-      column = @puzzle_string.each_line.map { |row| row.scan(/\d+/)[col].to_i }
-      # Exclude 0 when checking for duplicates
-      non_zero_values = column.reject { |value| value.zero? }
-      duplicates = non_zero_values.size > 1 && non_zero_values.size != non_zero_values.uniq.size
-
-      # Check if each number from 1 to 9 is present at most once in the column
-      contains_all_numbers = (1..9).all? { |num| column.count(num) <= 1 }
-
-      puts "Column #{col}: Non-zero values: #{non_zero_values}, Duplicates: #{duplicates ? 'Invalid' : 'Valid'}, Contains all numbers: #{contains_all_numbers} #{column}"
-
-      (!duplicates || non_zero_values.empty?) && contains_all_numbers
-    end
-    result
-  end
-
-  # Check if an array contains invalid numbers
-  def contains_invalid_numbers?(array)
-    array.any? { |value| value < 1 || value > 9 }
-  end
+  
 end
